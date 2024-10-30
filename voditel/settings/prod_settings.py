103fe98ede83e476751129a0509016cb6b5b3aa0
daboggg <vzinin@list.ru>
2024-10-30 11:02:28 +0300

    reset password finally

--- voditel/settings/prod_settings.py
+++ voditel/settings/prod_settings.py
@@ -23,3 +23,15 @@ DATABASES = {
 # }
 
 ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", default=[]).split(" ")
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+EMAIL_HOST = "smtp.yandex.ru"
+EMAIL_PORT = 465
+EMAIL_HOST_USER = "zvovan77@yandex.ru"
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+EMAIL_USE_SSL = True
+
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+SERVER_EMAIL = EMAIL_HOST_USER
+EMAIL_ADMIN = EMAIL_HOST_USER
